c846ff4df2e8bbc749e23a02ac45f00b560560ed
Robin Khiv <robinkhiv@gmail.com>
2019-02-11 09:36:36 -0800

    got input from submit

--- src/SearchAndFilter.js
+++ src/SearchAndFilter.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class SearchAndFilter extends React.Component {
+    constructor(props){
+        super();
+        this.state ={ 
+            inputValue: null};
+    }
+    handleSubmit(ev){
+        ev.preventDefault();
+        const val = this.state.inputValue;
+        console.log(val);
+    }    
+    titleChanged(title) { 
+        this.setState({ inputValue : title }); 
+    }
+
     render(){
         return (
             <form>
-                <label HTMLfor="searchTerm">Search: </label>
-                <input type="text" name="searchTerm" placeholder="henry" />
-                <input type="submit" name="submit" value="search" />
+                <label htmlFor="searchTerm">Search: </label>
+                <input type="text" name="searchTerm" id="search" placeholder="henry" value={this.state.inputValue} onChange={e => this.titleChanged(e.target.value)}/>
+                <input type="submit" name="submit" onClick={(e)=> this.handleSubmit(e)} value="search" />
             </form>
         )
     }
